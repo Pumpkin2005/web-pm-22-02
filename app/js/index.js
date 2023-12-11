5a789e3855383c742524d930a07d361b75bf7251
Mariia Harbuzenko <mariia.harbuzenko.pm.2022@lpnu.ua>
2023-12-11 19:56:05 +0200

    commit2

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,33 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "navbar-nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar-nav";
-    }
+// script.js
+
+// Функція для виклику AJAX
+function fetchData() {
+    // Виклик AJAX за допомогою Fetch API або іншого методу
+    fetch('your_api_endpoint')
+        .then(response => response.json())
+        .then(data => {
+            // data містить ваші дані, наприклад, { labels: [...], datasets: [...] }
+
+            // Виклик функції, яка рендерить графік
+            renderChart(data);
+        })
+        .catch(error => console.error('Error fetching data:', error));
 }
+
+// Функція для рендерингу графіка за допомогою Chart.js
+function renderChart(data) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    // data.labels - масив міток графіка
+    // data.datasets - масив наборів даних (див. документацію Chart.js для більш детальної інформації)
+    var myChart = new Chart(ctx, {
+        type: 'line', // Тип графіка (line, bar, pie, ін.)
+        data: {
+            labels: data.labels,
+            datasets: data.datasets
+        }
+    });
+}
+
+// Виклик функції при завантаженні сторінки
+fetchData();
